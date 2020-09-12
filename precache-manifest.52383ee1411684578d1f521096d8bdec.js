e5d6d703d8fc6032d720bd80b550db9495b87019
GuilleDA <42821868+GuilleDA@users.noreply.github.com>
2020-09-12 19:34:02 -0300

    aver

---
+++ precache-manifest.52383ee1411684578d1f521096d8bdec.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ecbaf60ed90d912bffdbebf1d29c86b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c21b4a3d59408e8fabc4",
+    "url": "/static/css/main.7b6b5456.chunk.css"
+  },
+  {
+    "revision": "bac593f83e05a51971ca",
+    "url": "/static/js/2.32c5ecdd.chunk.js"
+  },
+  {
+    "revision": "c80b09f21c3dd2137be347c65d78f97d",
+    "url": "/static/js/2.32c5ecdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c21b4a3d59408e8fabc4",
+    "url": "/static/js/main.3d7a06fc.chunk.js"
+  },
+  {
+    "revision": "17a34fb261de91b1b5c2",
+    "url": "/static/js/runtime-main.da86f32a.js"
+  },
+  {
+    "revision": "bfa9cab6e7889b72d8e27c7a1340f489",
+    "url": "/static/media/logo.bfa9cab6.svg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "ff60e0c882ad6bebd0d9bbfa0f4eff49",
+    "url": "/static/media/logo.ff60e0c8.png"
+  }
+]);
